cb25ff82614eff09dade60f772418ed5369c7c2f
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-24 00:36:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,3 @@
 class Artist 
-  attr_accessor :name ,:artist 
+  attr_accessor :name 
 
-def initialize (name)
-  @name = name
-  @artist = artist
-
-artist = Artist.new
-artist.name = "Beyonce"
- artist.name
- end
- end 
