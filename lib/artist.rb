eb8c38e2ec6e8f1be203aba3b19a45e3cc256663
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-24 00:17:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,15 @@
 class Artist 
   attr_accessor :name
+end
+def initialize (artist)
+  @artist = artist
+end
+artist = 
   
-  
-  
-#   describe "Artist" do 
-#   it "has a name" do
-#     artist = Artist.new
-#     artist.name = "Beyonce" 
-#     expect(artist.name).to eq("Beyonce")
-#   end
-# end
+describe "Artist" do 
+  it "has a name" do
+    artist = Artist.new
+    artist.name = "Beyonce" 
+    expect(artist.name).to eq("Beyonce")
+  end
+end
